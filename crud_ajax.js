d0acddea0ce2b6bc58b185c10419731fa78aa1fc
DiegoReyes99 <diego.reyes@interactive-expansion.us>
2021-04-22 17:24:22 -0500

    Commit de Ejercicios

---
+++ crud_ajax.js
@@ -0,0 +1,12 @@
+
+const d = document,
+
+$table = d.querySelector(".crud-table"),
+$form = d.querySelector(".crud-form"),
+$title = d.querySelector(".crud-title"),
+$template = d.getElementbyID(".crud-template")
+
+
+
+
+
---
+++ funciones.js
@@ -0,0 +1,3 @@
+const saludar = (nombre) => console.log(`Hola, me llamó ${nombre}`);
+
+saludar("Diego");
